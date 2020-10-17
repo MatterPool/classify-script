b040f92d06a67b8ee736eb0847aec276f95d7532
Harrison Beckerich <harrison@beckerich.org>
2020-10-16 22:32:42 -0700

    main added is-twetch-out

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const isPaymentOut = require('./is-payment-out');
-const isDataOut = require('./is-data-out');
-const is21e8Out = require('./is-21e8-out');
-const isBoostOut = require('./is-boost-out');
+const isPaymentOut = require('./lib/is-payment-out');
+const isDataOut = require('./lib/is-data-out');
+const is21e8Out = require('./lib/is-21e8-out');
+const isBoostOut = require('./lib/is-boost-out');
+const isTwetchOut = require('./lib/is-twetch-out');
 
-const classify = script => {
-  let tag = '';
+const classify = (script) => {
+	let tag = '';
 
-  if (isPaymentOut(script)) {
-    tag = 'payment';
-  } else if (isDataOut(script)) {
-    tag = 'data';
-  } else if (is21e8Out(script)) {
-    tag = '21e8';
-  } else if (isBoostOut(script)) {
-    tag = 'boost';
-  }
+	if (isPaymentOut(script)) {
+		tag = 'payment';
+	} else if (isDataOut(script)) {
+		tag = 'data';
 
-  return tag;
+		const twetch = isTwetchOut(script);
+
+		if (twetch) {
+			tag = twetch;
+		}
+	} else if (is21e8Out(script)) {
+		tag = '21e8';
+	} else if (isBoostOut(script)) {
+		tag = 'boost';
+	}
+
+	return tag;
 };
+
+module.exports = classify;
